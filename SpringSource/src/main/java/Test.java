972d5edf7fd4f9c9578b2710ce5e2843416c5203
cang.lv <lvcang224@163.com>
2018-11-09 19:55:07 +0800

    update

--- SpringSource/src/main/java/Test.java
+++ SpringSource/src/main/java/Test.java
@@ -6,8 +6,20 @@
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Test {
     private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
 
+    public static void main(String[] args) {
+        String aa="${cdscs},${bbbb}";
+        Pattern p=Pattern.compile("\\$\\{(.*?)\\}");
+        Matcher m=p.matcher(aa);
+        while(m.find())
+        {
+            System.out.println(m.group(1));
+        }
+    }
 
 }
