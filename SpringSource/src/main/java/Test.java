ed5a254b6c2c5ab955765fa352e30ac846bcbdc6
cang.lv <lvcang224@163.com>
2018-11-09 18:08:00 +0800

    spring begin

---
+++ SpringSource/src/main/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author cang.lv
+ * @date 2018/11/9
+ */
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Test {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Test.class);
+
+
+}
